6c0f7085f437476dde375647c69b88a384d76927
JenkenK <jenkenkwok@gmail.com>
2020-11-26 16:17:12 +0000

    created runner file with simple game adventure

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,7 @@ import components.Weapon;
 import creature.Creature;
 import magicians.Wizard;
 import rooms.Dungeon;
+import treasure.Treasure;
 
 import java.util.Scanner;
 
@@ -10,11 +11,38 @@ public class Runner {
         Scanner scanner = new Scanner(System.in);
 
         Dungeon dungeon = new Dungeon("Deep dark dungeon");
-        Creature orge = new Creature("orge", 50);
+        Creature ogre = new Creature("Ogre", 50);
         Weapon spell = new Weapon("Fireball", 10);
-        Wizard jenken = new Wizard("Jenken", 100, "Stick", orge, spell);
+        Wizard jenken = new Wizard("Jenken", 100, "Stick", ogre, spell);
 
+        System.out.println("Welcome to DnD!");
+        System.out.println("You are a wizard called: " + jenken.getName());
+        System.out.println("You are in a room called: " + dungeon.getName());
+
+        dungeon.addTreasure(Treasure.DIAMONDS);
+        dungeon.addCreature(ogre);
+
+        System.out.println("You are faced with a " + dungeon.getEnemies().get(0).getCreatureType() + " with " + ogre.getHealthPoints() + " health points");
+        System.out.println("You attack with: " + jenken.getSpell().getType());
+
+        while (dungeon.getEnemies().get(0).getHealthPoints() > 0){
+            jenken.attack(spell, ogre);
+            System.out.println("Ogre health is " + dungeon.getEnemies().get(0).getHealthPoints());
+            System.out.println("You attack with: " + jenken.getSpell().getType());
+        }
+
+        System.out.println("You killed the enemy!");
+        System.out.println("There are " + dungeon.getTreasureHoard().get(0) + " in the room.");
+        System.out.println("You collect the treasure in the room");
+
+        jenken.collectTreasure(dungeon.getTreasureHoard().get(0), dungeon);
+        System.out.println("You have " + jenken.getTreasuresInBag().get(0) + " in your bag.");
+
+        int total = 0;
+        for (Treasure treasure: jenken.getTreasuresInBag()){
+            total += treasure.getValue();
+        }
+
+        System.out.println("The total value of treasure in your bag is " + total);
     }
-
-
 }
