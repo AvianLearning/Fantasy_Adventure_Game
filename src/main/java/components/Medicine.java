494fc2fbb3cba093d2503636022168a77a13dab8
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-26 11:41:11 +0000

    medicine class and tests added for getters.

--- src/main/java/components/Medicine.java
+++ src/main/java/components/Medicine.java
@@ -1,4 +1,22 @@
 package components;
 
 public class Medicine {
+
+    private String potionName;
+    private int healPoints;
+
+    public Medicine(String potionName, int healPoints) {
+        this.potionName = potionName;
+        this.healPoints = healPoints;
+    }
+
+    public String getPotionName() {
+        return potionName;
+    }
+
+      public int getHealPoints() {
+        return healPoints;
+    }
+
+
 }
--- src/test/java/MedicineTest.java
+++ src/test/java/MedicineTest.java
@@ -1,4 +1,4 @@
-import components.Weapon;
+import components.Medicine;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,6 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 public class MedicineTest {
 
+    Medicine medicine;
+
+    @Before
+    public void setUp(){
+        medicine = new Medicine("Love Potion", 9);
+    }
+
+    @Test
+    public void canGetPotionName(){
+        assertEquals("Love Potion", medicine.getPotionName());
+    }
+
+    @Test
+    public void canGetHealPoints(){
+        assertEquals(9, medicine.getHealPoints());
+    }
 
 
 
