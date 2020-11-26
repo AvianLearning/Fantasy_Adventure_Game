19d66ba0ef21d531974762bb35bffc9a70418224
JenkenK <jenkenkwok@gmail.com>
2020-11-26 11:58:01 +0000

    created player abstract class

---
+++ src/main/java/Player.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public abstract class Player {
+
+    private String name;
+    private int healthPoints;
+    private ArrayList<Treasure> treasuresInBag;
+    private ArrayList<Creature> enemiesDefeated;
+
+    public Player(String name, int healthPoints) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.treasuresInBag = new ArrayList<>();
+        this.enemiesDefeated = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public ArrayList<Treasure> getTreasuresInBag() {
+        return treasuresInBag;
+    }
+
+    public ArrayList<Creature> getEnemiesDefeated() {
+        return enemiesDefeated;
+    }
+}
