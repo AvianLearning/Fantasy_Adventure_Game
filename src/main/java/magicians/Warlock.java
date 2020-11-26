d95d99cfd260c644a997fbef1a1afbbf261c9755
AvianLearning <iaradcliffe@hotmail.com>
2020-11-26 13:50:32 +0000

    Added warlock class

--- src/main/java/magicians/Warlock.java
+++ src/main/java/magicians/Warlock.java
@@ -1,4 +1,25 @@
 package magicians;
 
-public class Warlock {
+import components.Weapon;
+import creature.Creature;
+import player.Player;
+
+public class Warlock extends Player {
+
+    private Creature mythicalCreature;
+    private Weapon spell;
+
+    public Warlock(String name, int healthPoints, Creature mythicalCreature, Weapon spell) {
+        super(name, healthPoints);
+        this.mythicalCreature = mythicalCreature;
+        this.spell = spell;
+    }
+
+    public Creature getMythicalCreature() {
+        return mythicalCreature;
+    }
+
+    public Weapon getSpell() {
+        return spell;
+    }
 }
